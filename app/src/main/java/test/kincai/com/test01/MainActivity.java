e042dd24ad87eff5204e2b6526a282eb8917ca82
hiongyend <171970918@qq.com>
2017-04-09 11:44:24 +0800

    添加dataBinding

--- app/src/main/java/test/kincai/com/test01/MainActivity.java
+++ app/src/main/java/test/kincai/com/test01/MainActivity.java
@@ -1,13 +1,18 @@
 package test.kincai.com.test01;
 
+import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import test.kincai.com.test01.databinding.ActivityMainBinding;
+
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        ActivityMainBinding mBinding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        mBinding.setName(getString(R.string.app_name));
     }
 }
