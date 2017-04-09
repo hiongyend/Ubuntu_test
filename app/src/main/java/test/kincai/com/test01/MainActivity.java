1e49e9d5d28891d8772307c3e729ceca0c9c4397
hiongyend <171970918@qq.com>
2017-04-09 11:52:18 +0800

    MainActivity Log打印导包 加分好

--- app/src/main/java/test/kincai/com/test01/MainActivity.java
+++ app/src/main/java/test/kincai/com/test01/MainActivity.java
@@ -3,6 +3,7 @@ package test.kincai.com.test01;
 import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 import test.kincai.com.test01.databinding.ActivityMainBinding;
 
@@ -14,6 +15,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         ActivityMainBinding mBinding = DataBindingUtil.setContentView(this, R.layout.activity_main);
         mBinding.setName(getString(R.string.app_name));
-        Log.e("MainActivity", "hello world")
+        Log.e("MainActivity", "hello world");
     }
 }
